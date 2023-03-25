00be52f7264389474485adb0219074af783f57c8
OPZJY <102017305+LibreJiayi@users.noreply.github.com>
2023-03-25 20:40:59 +0800

    Update __init__.py

--- app/controller/__init__.py
+++ app/controller/__init__.py
@@ -1,9 +1,2 @@
 
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
-
-SQLALCHEMY_DATABASE_URI = "mysql+pymysql://dzj:Dzj1365878@rm-2zes1gmjlcey26osclo.mysql.rds.aliyuncs.com:3306/parking-system"
-engine = create_engine(SQLALCHEMY_DATABASE_URI)
-Session = sessionmaker(bind=engine)
-session = Session()
 
