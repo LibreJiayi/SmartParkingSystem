efb12d0218bead5c6219f79910088f1b1b7274b3
OPZJY <102017305+LibreJiayi@users.noreply.github.com>
2023-04-03 18:13:26 +0800

    Update __init__.py

--- app/controller/__init__.py
+++ app/controller/__init__.py
@@ -1,2 +1,12 @@
 
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+# 使用 SQLite 作为本地数据库
+SQLALCHEMY_DATABASE_URI = "sqlite:///your_local_database.db"
+engine = create_engine(SQLALCHEMY_DATABASE_URI)
+Session = sessionmaker(bind=engine)
+session = Session()
+
+
 
